f77ea98223d2382e81d82d472bed57b7f8c3476b
Tanel Hiob <tanelhiob@outlook.com>
2016-02-02 21:00:45 +0200

    Abstracted the program

--- TanelPiinabGame/Program.cs
+++ TanelPiinabGame/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TanelPiinabGame
 {
@@ -10,7 +6,7 @@ namespace TanelPiinabGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Tanel!");
+            Console.WriteLine($"Hello, { args[0] }");
         }
     }
 }

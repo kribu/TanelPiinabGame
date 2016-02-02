1080aa1a918e38d156b77e446a91d8a0206dd08f
Kristin Ehala <kristin.ehala@gmail.com>
2016-02-02 20:54:34 +0200

    Hello

---
+++ TanelPiinabGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TanelPiinabGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Tanel!");
+        }
+    }
+}

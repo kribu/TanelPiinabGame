ce75999c8bf6a85b0031687288361fca7f0e970b
Kristin Ehala <kristin.ehala@gmail.com>
2016-02-05 22:39:03 +0200

    kinky

---
+++ TanelPiinabGame/Monster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Monster{
+
+    public string Name{
+        get; set;
+    }
+
+    public int Health{
+        get; set;
+    }
+
+    public int HitValue{
+        get; set;
+    }
+
+    public void Hit(Monster firstMonster, Monster secondMonster){
+        Random rnd = new Random();
+        int whoNext = rnd.Next(101); 
+        if (whoNext % 2 == 0){
+            firstMonster.Health = firstMonster.Health - secondMonster.HitValue; // TODO: ära elusid negatiivseks lase - dont care, my game, my rules
+        }else{
+            secondMonster.Health = secondMonster.Health - firstMonster.HitValue; // TODO: ära elusid negatiivseks lase
+        }
+        Console.WriteLine($"{firstMonster.Name}  has {firstMonster.Health} lives left and {secondMonster.Name} has {secondMonster.Health} lives left"); 
+    }
+
+    public bool isAlive(Monster currentMonster){
+        if (currentMonster.Health > 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
